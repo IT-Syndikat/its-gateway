c7cbf9da3346787992b9ed1486fb71084b58c775
mindworksat <matthias+git@gwrx.at>
2015-11-17 23:37:04 +0100

    .

--- pingtest.sh
+++ pingtest.sh
@@ -2,7 +2,7 @@
 
 LOG="ping.log"
 
-while true; do
+#while true; do
 	ping -c 1 8.8.8.8 &> /dev/null
 	if [[ $? -eq 0 ]]; then
 		echo -n $(date) >> $LOG
@@ -12,4 +12,4 @@ while true; do
 		echo -e "\tnot working" >> $LOG
 	fi
 	sleep 1
-done
+#done
